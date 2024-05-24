c9b526b42ce90afb0c7af349e2874680b9039af5
Ana Beatriz Cabral Pinto <beatrizcabralp.2002@gmail.com>
2024-05-24 15:20:38 -0300

    comeco da semana 3

---
+++ semana 3/variaveis/script.js
@@ -0,0 +1,49 @@
+var nome ='Ana';
+var idade = 22;
+var falaingerus = true;
+
+console.log(nome, idade, falaingerus);
+
+var preco= 25;
+var quantidade = 7;
+var total = preco * quantidade;
+
+console.log(total);
+
+var sobrenome = 'Beatriz'
+     var cidade = 'Natal';
+
+console.log(sobrenome, cidade);
+
+var semdefinir;
+
+console.log(semdefinir);
+
+/*****exercicio */
+
+/* Declarar uma variavel com meu nome*/
+
+var meunome = 'Ana Beatriz';
+console.log(meunome);
+
+/* Declarar uma variavel com minha idade*/
+
+var minhaidade = 22;
+console.log(minhaidade);
+
+/* Declarar uma variavel com minha comida fav e nao atribuir valor*/
+
+var comidafav;
+
+/* Atibuir valor a sua comida*/
+
+comidafav = 'batata';
+console.log(comidafav);
+
+/*Declarar 5 variaveis diferentes sem valores*/
+
+var idiomas;
+var flores;
+var paises;
+var musicas;
+var comidas;
